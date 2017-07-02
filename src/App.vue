778f8fdf4114b030b6f9a6e9e423501e22307f24
Evgeniy Marthenov <evgeniy.marthenov@gmail.com>
2017-07-03 00:22:50 +0300

    first init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Timepicker></Timepicker>
+  </div>
+</template>
+
+<script>
+import Timepicker from './components/timepicker';
+
+export default {
+  name: 'app',
+  components: {
+    Timepicker,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/timepicker/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Time Picker from me
+  </div>
+</template>
